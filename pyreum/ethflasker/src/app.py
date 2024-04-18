728f04f9df2f70c3414f83e8226832768352d7ad
ddb0000 <dbarcelloz@gmail.com>
2024-04-18 20:09:17 -0300

    gui sorta

--- pyreum/ethflasker/src/app.py
+++ pyreum/ethflasker/src/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from blockchain import get_token_balance
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/balance/<address>')
 def balance(address):
